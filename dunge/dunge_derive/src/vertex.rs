9da21b0e0bc1e0e252aad51ba1b5c76d85bd7362
nanoqsh <nanoqsh@gmail.com>
2023-05-16 03:27:11 +0600

    Updated derive errors

--- dunge/dunge_derive/src/vertex.rs
+++ dunge/dunge_derive/src/vertex.rs
@@ -1,49 +1,59 @@
 use {
     proc_macro2::{Ident, Span, TokenStream},
-    syn::{Attribute, Data, DataStruct, DeriveInput, Field},
+    syn::{spanned::Spanned, Attribute, Data, DataStruct, DeriveInput, Field},
 };
 
 pub(crate) fn impl_vertex(derive: DeriveInput) -> TokenStream {
     use quote::ToTokens;
 
     let Data::Struct(DataStruct { fields, .. }) = derive.data else {
-        panic!("the vertex type must be a struct");
+        return quote::quote_spanned! { derive.ident.span() =>
+            ::std::compile_error!("the vertex type must be a struct");
+        };
     };
 
-    assert!(
-        !fields.is_empty(),
-        "the vertex struct must have some fields",
-    );
+    let fields_span = fields.span();
+    if fields.is_empty() {
+        return quote::quote_spanned! { fields_span =>
+            ::std::compile_error!("the vertex struct must have some fields");
+        };
+    }
 
-    assert!(
-        derive.generics.params.is_empty(),
-        "the vertex struct cannot have generic parameters",
-    );
+    if !derive.generics.params.is_empty() {
+        return quote::quote_spanned! { derive.generics.params.span() =>
+            ::std::compile_error!("the vertex struct cannot have generic parameters");
+        };
+    }
 
-    assert!(
-        derive.attrs.iter().any(is_repr_c),
-        "the vertex struct must have the `#[repr(C)]` attribute",
-    );
+    if !derive.attrs.iter().any(is_repr_c) {
+        return quote::quote_spanned! { derive.ident.span() =>
+            ::std::compile_error!("the vertex struct must have the `#[repr(C)]` attribute");
+        };
+    }
 
     let mut has_position = false;
     let fields: Vec<_> = (0..)
         .zip(fields)
         .map(|(n, field)| {
-            let Some(kind) = field_kind(&field) else {
-                panic!(
+            let span = field.ident.span();
+            let Some(kind) = Kind::from_field(&field) else {
+                let msg = format!(
                     "the `{name}` field must be specified by some vertex attribute",
                     name = match field.ident {
                         Some(ident) => ident.to_string(),
                         _ => n.to_string(),
                     },
                 );
+
+                return quote::quote_spanned! { span => ::std::compile_error!(#msg) }
             };
 
             if let Kind::Position = kind {
-                assert!(
-                    !has_position,
-                    "fields must have only one `#[position]` attribute",
-                );
+                if has_position {
+                    return quote::quote_spanned! { span =>
+                        ::std::compile_error!("fields must have only one `#[position]` attribute")
+                    };
+                }
 
                 has_position = true;
             }
@@ -56,7 +66,7 @@ pub(crate) fn impl_vertex(derive: DeriveInput) -> TokenStream {
             );
 
             let kind = kind.into_ident();
-            quote::quote! {{
+            quote::quote_spanned! { span => {
                 let f = ::dunge::vertex::Field {
                     kind: ::dunge::vertex::Kind::#kind,
                     format: ::dunge::vertex::component_format::<#ty>(),
@@ -68,10 +78,11 @@ pub(crate) fn impl_vertex(derive: DeriveInput) -> TokenStream {
         })
         .collect();
 
-    assert!(
-        has_position,
-        "some field must have the `#[position]` attribute",
-    );
+    if !has_position {
+        return quote::quote_spanned! { fields_span =>
+            ::std::compile_error!("some field must have the `#[position]` attribute");
+        };
+    }
 
     let type_name = derive.ident;
     quote::quote! {
@@ -103,6 +114,17 @@ enum Kind {
 }
 
 impl Kind {
+    fn from_field(field: &Field) -> Option<Self> {
+        use syn::Meta;
+
+        field.attrs.iter().find_map(|attr| match &attr.meta {
+            Meta::Path(path) if path.is_ident("position") => Some(Self::Position),
+            Meta::Path(path) if path.is_ident("color") => Some(Self::Color),
+            Meta::Path(path) if path.is_ident("texture_map") => Some(Self::TextureMap),
+            _ => None,
+        })
+    }
+
     fn into_ident(self) -> Ident {
         let s = match self {
             Self::Position => "Position",
@@ -120,15 +142,4 @@ impl Kind {
             Self::TextureMap => "texture_map",
         }
     }
-}
-
-fn field_kind(field: &Field) -> Option<Kind> {
-    use syn::Meta;
-
-    field.attrs.iter().find_map(|attr| match &attr.meta {
-        Meta::Path(path) if path.is_ident("position") => Some(Kind::Position),
-        Meta::Path(path) if path.is_ident("color") => Some(Kind::Color),
-        Meta::Path(path) if path.is_ident("texture_map") => Some(Kind::TextureMap),
-        _ => None,
-    })
 }
