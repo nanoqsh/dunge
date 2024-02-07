eea9a94c937423420850758aa4d12aee0aa8aad5
nanoqsh <nanoqsh@gmail.com>
2024-02-07 18:48:50 +0600

    Run wasm

--- examples/run_wasm/src/main.rs
+++ examples/run_wasm/src/main.rs
@@ -1,16 +1,43 @@
-fn main() {
-    use helpers::serv::{self, Page};
+type Error = Box<dyn std::error::Error>;
 
-    serv::run(&[
-        ("/", Page::html(include_str!("../static/index.html"))),
+fn main() {
+    if let Err(err) = run() {
+        eprintln!("error: {err}");
+    }
+}
+
+fn run() -> Result<(), Error> {
+    use {
+        askama::Template,
+        helpers::serv::{self, Page},
+        std::{env, fs},
+    };
+
+    let module = env::args().nth(1).ok_or("no module specified")?;
+    let index = |module| {
+        #[derive(Template)]
+        #[template(path = "index.html")]
+        struct Index<'a> {
+            module: &'a str,
+        }
+
+        Index { module }
+    };
+
+    let html = index(&module).render()?.leak();
+    let prefix = "examples/run_wasm/static";
+    let strip = |s: &'static str| -> &'static str { s.strip_prefix(prefix).expect("strip") };
+    let js_path = format!("{prefix}/{module}.js").leak();
+    let wasm_path = format!("{prefix}/{module}_bg.wasm").leak();
+    let js = fs::read_to_string(&js_path)?.leak();
+    let wasm = fs::read(&wasm_path)?.leak();
+    let routes = &[
+        ("/", Page::html(html)),
         ("/favicon.ico", Page::html("")),
-        // (
-        //     "/triangle.js",
-        //     Page::js(include_str!("../static/triangle.js")),
-        // ),
-        // (
-        //     "/triangle_bg.wasm",
-        //     Page::wasm(include_bytes!("../static/triangle_bg.wasm")),
-        // ),
-    ]);
+        (strip(js_path), Page::js(js)),
+        (strip(wasm_path), Page::wasm(wasm)),
+    ];
+
+    serv::run(routes);
+    Ok(())
 }
